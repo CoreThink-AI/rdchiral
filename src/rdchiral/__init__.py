c52eaa3b963970f552420b52657cc1dd2f40159f
hobs <git@totalgood.com>
2026-06-12 11:24:35 -0700

    __init__

--- src/rdchiral/__init__.py
+++ src/rdchiral/__init__.py
@@ -1,11 +1,13 @@
-try:
-	import rdkit
-except:
-	raise ImportError('RDKit cannot be imported. rdchiral is a wrapper for RDKit, instructions for installing RDKit can be found at https://www.rdkit.org/docs/Install.html')
-if rdkit.__version__ < '2019':
-	raise ImportWarning('RDKit version {} may not be compatable with rdchiral. Upgrade to RDKit version 2019 or higher.'.format(rdkit.__version__))
-
+import rdkit
 from rdkit import RDLogger
+
+from rdchiral import bonds
 
 lg = RDLogger.logger()
 lg.setLevel(RDLogger.CRITICAL)
+
+if rdkit.__version__ < '2019':
+    raise ImportWarning('RDKit version {} may not be compatable with rdchiral. Upgrade to RDKit version 2019 or higher.'.format(rdkit.__version__))
+
+
+__all__ = ["bonds"]
